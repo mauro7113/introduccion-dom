89b8481817cdbb741d3d06ea42ed28a846515549
mauro7113 <mauro7113@hotmail.com>
2024-10-02 16:09:50 -0500

    avance en el proyecto

---
+++ view/public/js/script.js
@@ -0,0 +1,19 @@
+/*Selectores */
+/*getElementById -> Selecciona elemento por un IdleDeadline*/
+/*getElementByclassName -> Seleccione elementos por clase*/
+/*getElementByTagName -> Selecciona elemento por nombre de etiqueta*/
+
+querySelector
+/*    querySelector -> permite seleccionar por Id, por clase y por etiqueta
+    querySelectorAll -> permite seleccionar por id, por clase y por etiqueta*/
+
+
+
+/*document.getElementById()*/
+
+console.log(document.getElementById("usuario"))/*solo muestra un elemento */
+console.log(document.getElementsByClassName("form"))/*siempre muestra un arreglo */
+console.log(document.getElementsByTagName("form"))/*siempre muestra un arreglo */
+
+console.log(document.querySelector("form"))/* */
+console.log(document.querySelectorAll("form"))
